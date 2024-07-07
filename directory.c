d4617304c0bee4f41c198d1f288a807802581061
student <bryanlee56098@gmail.com>
2024-07-07 16:30:51 -0700

    Initial config

---
+++ directory.c
@@ -0,0 +1,52 @@
+#include "Directory.h"
+
+DirectoryEntry *initDirectory(int minEntries, DirectoryEntry *parent)
+{
+    int bytesNeeded = minEntries * sizeof(DirectoryEntry);
+    int blocksNeeded = (bytesNeeded + BLOCKSIZE - 1) / BLOCKSIZE;
+    int bytesToALloc = bytesNeeded * BLOCKSIZE;
+    DirectoryEntry *DEs = malloc(bytesToALloc);
+
+    if (DEs == NULL)
+    {
+        // Handle memory allocation failure
+        return NULL;
+    }
+
+    int actualEntries = bytesToALloc / sizeof(DirectoryEntry);
+    // TODO: init all the actual entries
+    for(int i = 2; i < actualEntries; i++) {
+        time_t currentTime = time(NULL);
+        DEs[i].dateCreated = currentTime;
+        DEs[i].dateModified = currentTime;
+        strcpy(DEs[i].name, "");
+        DEs[i].isDirectory = ' ';
+        DEs[i].size = 0;
+        DEs[i].location = -1;
+        DEs[i].permissions = 0;
+    }
+
+    // Initialize . and ..
+    int newLoc = 100; // TODO: Replace with fsalloc(blocksNeeded)
+    time_t currentTime = time(NULL);
+
+    // Initialize "."
+    DEs[0].location = newLoc;
+    DEs[0].size = actualEntries;
+    strcpy(DEs[0].name, ".");
+    DEs[0].isDirectory = 'd';
+    DEs[0].dateCreated = currentTime;
+    DEs[0].dateModified = currentTime;
+    DEs[0].permissions = 0;
+
+    DirectoryEntry *dotdot = parent;
+    if (dotdot == NULL)
+    {
+        dotdot = &DEs[0];
+    }
+
+    memcpy(&DEs[1], &DEs[0], sizeof(DirectoryEntry));
+    strcpy(DEs[0].name, "..");
+
+    return DEs;
+}
---
+++ fsDesign.h
@@ -0,0 +1,34 @@
+#ifndef _FSDES_H
+#define _FSDES_H
+#include <time.h>
+
+typedef struct DirectoryEntry
+{
+   time_t dateCreated;     // seconds since epoch
+   time_t dateModified;    // seconds since epoch
+
+   char name[51];          // directory entry name
+   char isDirectory;       // is directory entry a directory
+
+   int size;               // directory entry size
+   int location;           // starting block number for the file data
+   int permissions;        // file modes converted to decimal format
+  
+   // total size = 80, divisible by 16, and no internal paddings
+} DirectoryEntry;
+
+
+typedef struct VCB
+{
+   long signature;         // to identify VCB
+
+   int blockSize;          // size of each block
+   int totalBlocks;        // total blocks in volume
+
+   int freeSpaceLocation;  // location to free space block
+   int totalFreeSpace;     // total free blocks
+
+   int rootLocation;       // location to root directory
+} VCB;
+
+#endif
