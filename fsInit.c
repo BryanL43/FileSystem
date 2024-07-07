4bcc6f9af715e2d118e4c98ecea57730e24e2c8a
student <bryanlee56098@gmail.com>
2024-07-07 16:33:47 -0700

    whoops

--- fsInit.c
+++ fsInit.c
@@ -25,7 +25,7 @@
 #include "mfs.h"
 
 
-int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize) {
+int initFileSystem(uint64_t numberOfBlocks, uint64_t blockSize) {
 	printf ("Initializing File System with %ld blocks with a block size of %ld\n", numberOfBlocks, blockSize);
 	/* TODO: Add any code you need to initialize your file system. */
 
@@ -33,6 +33,6 @@ int initFileSystem (uint64_t numberOfBlocks, uint64_t blockSize) {
 }
 
 	
-void exitFileSystem () {
+void exitFileSystem() {
 	printf ("System exiting\n");
 }
