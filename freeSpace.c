ab0a021d40336659867c6bf7f37b03407644a8a1
student <klam11@sfsu.edu>
2024-07-07 18:50:20 -0700

    Finsihed writing innit function

--- freeSpace.c
+++ freeSpace.c
@@ -2,15 +2,33 @@
 #include "fsDesign.h"
 
 int initFreeSpace(uint64_t numberOfBlocks, uint64_t blockSize) {
-    //(n+m-1)/m int cast
-    // int blocksNeeded = (int)numberOfBlocks
-    printf("I have fired\n");
+    int blocksNeeded = (numberOfBlocks + blockSize -1)/blockSize;
+    int *FAT = malloc(blocksNeeded*blockSize);
 
-    return 0;
+    FAT[0] = 0xFFFFFFFF;
+    for (int i = 1; i < numberOfBlocks; i++) {
+        FAT[i] = i+1;
+    }
+
+    // Sentinel value for FAT storage
+    FAT(blocksNeeded) = 0xFFFFFFFF;
+
+    // Sentinel value for free space storage 
+    FAT(numberOfBlocks) = 0xFFFFFFFF
+
+    int blocksWritten = LBAwrite(FAT, blocksNeeded, 1);
+    // TO DO: Update VCB values after initializing VCB
+
+    if (blocksWritten == -1) {
+        return -1;
+    } else {
+        return blocksNeeded + 1; // index at the end of FAT block + 1
+    }
+
 }
 
 int getFreeBlocks(uint64_t numberOfBlocks) {
-    
+
 }
 
 int writeBlock(uint64_t numberOfBlocks, void * buffer, int location) {
