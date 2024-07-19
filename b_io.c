97d96cbdc0aac6f563e357a5bedf7fee2a643b35
student <bryanlee56098@gmail.com>
2024-07-19 00:42:27 -0700

    started b_open (handles create flag only atm)

--- b_io.c
+++ b_io.c
@@ -21,53 +21,53 @@
 #include <fcntl.h>
 #include "b_io.h"
 #include "freeSpace.h"
+#include "fsUtils.h"
 
 #define MAXFCBS 20
 #define B_CHUNK_SIZE 512
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 
-typedef struct b_fcb
-	{
+typedef struct b_fcb {
+	DirectoryEntry* fi; //holds the file information
+
 	/** TODO add al the information you need in the file control block **/
-	char * buf;		//holds the open file buffer
-	int index;		//holds the current position in the buffer
-	int buflen;		//holds how many valid bytes are in the buffer
-	} b_fcb;
+	char* buf;			//holds the open file buffer
+	int index;			//holds the current position in the buffer
+	int buflen;			//holds how many valid bytes are in the buffer
+
+	int fileIndex;		//holds the index at where the file is located at in the parent
+} b_fcb;
 	
 b_fcb fcbArray[MAXFCBS];
 
 int startup = 0;	//Indicates that this has not been initialized
 
 //Method to initialize our file system
-void b_init ()
-	{
+void b_init() {
 	//init fcbArray to all free
-	for (int i = 0; i < MAXFCBS; i++)
-		{
+	for (int i = 0; i < MAXFCBS; i++) {
 		fcbArray[i].buf = NULL; //indicates a free fcbArray
-		}
+	}
 		
 	startup = 1;
-	}
+}
 
 //Method to get a free FCB element
-b_io_fd b_getFCB ()
-	{
-	for (int i = 0; i < MAXFCBS; i++)
-		{
-		if (fcbArray[i].buff == NULL)
-			{
+b_io_fd b_getFCB() {
+	for (int i = 0; i < MAXFCBS; i++) {
+		if (fcbArray[i].buf == NULL) {
 			return i;		//Not thread safe (But do not worry about it for this assignment)
-			}
 		}
-	return (-1);  //all in use
 	}
+	return (-1); //all in use
+}
 	
 // Interface to open a buffered file
 // Modification of interface for this assignment, flags match the Linux flags for open
 // O_RDONLY, O_WRONLY, or O_RDWR
 b_io_fd b_open(char* filename, int flags) {
 	b_io_fd returnFd;
+	ppInfo ppi;
 
 	//*** TODO ***:  Modify to save or set any information needed
 	//
@@ -76,46 +76,87 @@ b_io_fd b_open(char* filename, int flags) {
 	if (startup == 0) b_init();  //Initialize our system
 	
 	returnFd = b_getFCB(); // get our own file descriptor
-	if (returnFD == -1) { // check for error - all used FCB's
-		return returnFD;
+	if (returnFd == -1) { // check for error - all used FCB's
+		return returnFd;
 	}
+
+	if (parsePath(filename, &ppi) == -1) {
+        return -1;
+    }
+
+	b_fcb fcb = fcbArray[returnFd];
+	fcb.fi = malloc(sizeof(DirectoryEntry));
+	fcb.buf = malloc(vcb->blockSize);
 	
+	// Create a new file
+	if (flags & O_CREAT) {
+		strncpy(fcb.fi->name, ppi.lastElement, sizeof(fcb.fi->name));
+		fcb.fi->size = 0;
+		fcb.fi->location = -2; // Special sentinel to indicate file occupied space
+		fcb.fi->isDirectory = ' ';
+		time_t currTime = time(NULL);
+		fcb.fi->dateCreated = currTime;
+		fcb.fi->dateModified = currTime;
+
+		//[TO-DO] update variable information needed for read/write
+
+		DirectoryEntry* temp = loadDir(ppi.parent);
+    	ppi.parent = temp;
+
+		fcb.fileIndex = findUnusedDE(ppi.parent);
+		if (fcb.fileIndex == -1) {
+			ppi.parent = expandDirectory(ppi.parent);
+        	fcb.fileIndex = findUnusedDE(ppi.parent);
+		}
+
+		// Fatal error if no vacant entry spot is found
+		if (fcb.fileIndex < 0) {
+			free(fcb.fi);
+			free(fcb.buf);
+			free(temp);
+			return -1;
+		}
+
+		free(temp);
+	}
+
+	ppi.parent[fcb.fileIndex] = *fcb.fi; //dereference file information and write to disk
+	
+	// Write file to disk
+	int numOfBlocks = (ppi.parent->size + vcb->blockSize - 1) / vcb->blockSize;
+	writeBlock(ppi.parent, numOfBlocks, ppi.parent[0].location);
+
+	fcbArray[returnFd] = fcb;
 	
 	return (returnFd); // all set
 }
 
 
 // Interface to seek function	
-int b_seek (b_io_fd fd, off_t offset, int whence)
-	{
-	if (startup == 0) b_init();  //Initialize our system
+int b_seek (b_io_fd fd, off_t offset, int whence) {
+	if (startup == 0) b_init(); //Initialize our system
 
 	// check that fd is between 0 and (MAXFCBS-1)
-	if ((fd < 0) || (fd >= MAXFCBS))
-		{
-		return (-1); 					//invalid file descriptor
-		}
-		
-		
-	return (0); //Change this
+	if ((fd < 0) || (fd >= MAXFCBS)) {
+		return (-1); //invalid file descriptor
 	}
+
+	return (0); //Change this
+}
 
 
 
 // Interface to write function	
-int b_write (b_io_fd fd, char * buffer, int count)
-	{
+int b_write (b_io_fd fd, char * buffer, int count) {
 	if (startup == 0) b_init();  //Initialize our system
 
 	// check that fd is between 0 and (MAXFCBS-1)
-	if ((fd < 0) || (fd >= MAXFCBS))
-		{
-		return (-1); 					//invalid file descriptor
-		}
-		
+	if ((fd < 0) || (fd >= MAXFCBS)) {
+		return (-1); //invalid file descriptor
+	}
 		
 	return (0); //Change this
-	}
+}
 
 
 
@@ -138,94 +179,98 @@ int b_write (b_io_fd fd, char * buffer, int count)
 //  |             |                                                |        |
 //  | Part1       |  Part 2                                        | Part3  |
 //  +-------------+------------------------------------------------+--------+
-int b_read (b_io_fd fd, char * buffer, int count)
-	{
+int b_read (b_io_fd fd, char * buffer, int count) {
 
-
-
-if (startup == 0)
-		b_init(); // Initialize our system
+	if (startup == 0) b_init(); // Initialize our system
 
 	// check that fd is between 0 and (MAXFCBS-1)
-	if ((fd < 0) || (fd >= MAXFCBS))
-	{
+	if ((fd < 0) || (fd >= MAXFCBS)) {
 		return (-1); // invalid file descriptor
 	}
 
 	// and check that the specified FCB is actually in use
-	if (fcbArray[fd].fi == NULL) // File not open for this descriptor
-	{
+	if (fcbArray[fd].fi == NULL) { // File not open for this descriptor 
 		return -1;
 	}
 
-	b_fcb *fcb = &fcbArray[fd];
-	int bytesRead = 0;
+	return 0;
 
-	int bytesRemainingInFile = fcb->fi->fileSize - fcb->position;
-	int bytesToRead = MIN(count, bytesRemainingInFile);
+// 	b_fcb *fcb = &fcbArray[fd];
+// 	int bytesRead = 0;
 
-	// At the end of the file
-	if (bytesToRead <= 0)
-		return 0;
+// 	int bytesRemainingInFile = fcb->fi->fileSize - fcb->position;
+// 	int bytesToRead = MIN(count, bytesRemainingInFile);
 
-	// Calculate current buffer position and bytes left in buffer
-	int bytesRemainingInBuffer = B_CHUNK_SIZE - fcb->bufferPosition;
-	int bytesToReadFromBuffer = MIN(bytesToRead, bytesRemainingInBuffer);
+// 	// At the end of the file
+// 	if (bytesToRead <= 0)
+// 		return 0;
 
-	// when there are already bytes in the buffer
-	if (bytesToReadFromBuffer > 0)
-	{
-		// Read any remaining bytes from a previous read into the buffer.
-		memcpy(buffer + bytesRead, fcb->buffer + fcb->bufferPosition, bytesToReadFromBuffer);
+// 	// Calculate current buffer position and bytes left in buffer
+// 	int bytesRemainingInBuffer = B_CHUNK_SIZE - fcb->bufferPosition;
+// 	int bytesToReadFromBuffer = MIN(bytesToRead, bytesRemainingInBuffer);
 
-		// update trackers
-		fcb->position += bytesToReadFromBuffer;
-		fcb->bufferPosition = fcb->position % B_CHUNK_SIZE;
-		bytesRead += bytesToReadFromBuffer;
-		bytesToRead -= bytesToReadFromBuffer;
-	}
+// 	// when there are already bytes in the buffer
+// 	if (bytesToReadFromBuffer > 0)
+// 	{
+// 		// Read any remaining bytes from a previous read into the buffer.
+// 		memcpy(buffer + bytesRead, fcb->buffer + fcb->bufferPosition, bytesToReadFromBuffer);
 
-	// Read full chunks directly to the user buffer
-	if (bytesToRead >= B_CHUNK_SIZE)
-	{
-		int fullChunksToRead = bytesToRead / B_CHUNK_SIZE;
-// NEED TO UPDATE CURRENT BLOCK TO FIND CORRECT BLOCK IN FAT
-		int currentBlock = (fcb->position / B_CHUNK_SIZE) + fcb->fi->location;
+// 		// update trackers
+// 		fcb->position += bytesToReadFromBuffer;
+// 		fcb->bufferPosition = fcb->position % B_CHUNK_SIZE;
+// 		bytesRead += bytesToReadFromBuffer;
+// 		bytesToRead -= bytesToReadFromBuffer;
+// 	}
 
-		// Read as many chunks needed ant calculate the bytes that were read to update trackers
-		int chunksRead = readBlock(fcb->buf, fullChunksToRead,	currentBlock);
-		if (chunksRead < 1) {
-			return -1;
-		}
-		int bytesReadFromChunks = chunksRead * B_CHUNK_SIZE;
+// 	// Read full chunks directly to the user buffer
+// 	if (bytesToRead >= B_CHUNK_SIZE)
+// 	{
+// 		int fullChunksToRead = bytesToRead / B_CHUNK_SIZE;
+// // NEED TO UPDATE CURRENT BLOCK TO FIND CORRECT BLOCK IN FAT
+// 		int currentBlock = (fcb->position / B_CHUNK_SIZE) + fcb->fi->location;
 
-		// update trackers
-		fcb->position += bytesReadFromChunks;
-		fcb->bufferPosition = fcb->position % B_CHUNK_SIZE;
-		bytesRead += bytesReadFromChunks;
-		bytesToRead -= bytesReadFromChunks;
-	}
+// 		// Read as many chunks needed ant calculate the bytes that were read to update trackers
+// 		int chunksRead = readBlock(fcb->buf, fullChunksToRead,	currentBlock);
+// 		if (chunksRead < 1) {
+// 			return -1;
+// 		}
+// 		int bytesReadFromChunks = chunksRead * B_CHUNK_SIZE;
 
-	// Read any remaining bytes into the buffer and copy them to the user buffer
-	if (bytesToRead > 0)
-	{
-		int currentBlock = (fcb->position / B_CHUNK_SIZE) + fcb->fi->location;
-// NEED TO UPDATE CURRENT BLOCK TO FIND CORRECT BLOCK IN FAT
-		if(readBlock(fcb->buffer, 1, currentBlock) != 1) {
-			return -1;
-		}
-		memcpy(buffer + bytesRead, fcb->buffer, bytesToRead);
+// 		// update trackers
+// 		fcb->position += bytesReadFromChunks;
+// 		fcb->bufferPosition = fcb->position % B_CHUNK_SIZE;
+// 		bytesRead += bytesReadFromChunks;
+// 		bytesToRead -= bytesReadFromChunks;
+// 	}
 
-		// update trackers
-		fcb->position += bytesToRead;
-		fcb->bufferPosition = fcb->position % B_CHUNK_SIZE;
-		bytesRead += bytesToRead;
-	}
+// 	// Read any remaining bytes into the buffer and copy them to the user buffer
+// 	if (bytesToRead > 0)
+// 	{
+// 		int currentBlock = (fcb->position / B_CHUNK_SIZE) + fcb->fi->location;
+// // NEED TO UPDATE CURRENT BLOCK TO FIND CORRECT BLOCK IN FAT
+// 		if(readBlock(fcb->buffer, 1, currentBlock) != 1) {
+// 			return -1;
+// 		}
+// 		memcpy(buffer + bytesRead, fcb->buffer, bytesToRead);
 
-	return bytesRead;
+// 		// update trackers
+// 		fcb->position += bytesToRead;
+// 		fcb->bufferPosition = fcb->position % B_CHUNK_SIZE;
+// 		bytesRead += bytesToRead;
+// 	}
+
+// 	return bytesRead;
 	}
 	
 // Interface to Close the file	
 int b_close (b_io_fd fd) {
-	
+	if (fd < 0 || fd >= MAXFCBS || fcbArray[fd].fi == NULL) {
+		return -1;
+	}
+
+	fcbArray[fd].fi = NULL;
+	free(fcbArray[fd].buf);
+	fcbArray[fd].buf = NULL;
+
+	return 0;
 }
